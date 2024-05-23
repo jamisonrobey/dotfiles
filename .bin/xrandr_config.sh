037d98470e3d8e566463043fe4bd8c34fed703a6
Jamison Robey <jamisonrobey@gmail.com>
2024-05-23 02:52:36 +0000

    xrandr

--- .bin/xrandr_config.sh
+++ .bin/xrandr_config.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-xrandr --output DP-2 --primary --mode 1920x1080 --rate 144.00 --output HDMI-0 --left-of DP-2 --output DVI-D-0 --right-of DP-2
+xrandr --output DP-2 --primary --mode 1920x1080 --rate 144.00 --output HDMI-1 --rate 74.97 --left-of DP-2 --output DVI-D-1 --right-of DP-2
 
